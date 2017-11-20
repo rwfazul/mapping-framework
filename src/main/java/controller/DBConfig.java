017d3f462f621a45c7a2744069d7fd5a021fc923
rwfazul <rhauaita@hotmail.com>
2017-11-20 19:15:58 -0200

    add classe cofig. chaveamento sgbds

---
+++ src/main/java/controller/DBConfig.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import banco.RegistroDAO;
+import model.dao.mongo.EventoDAOMongo;
+import model.dao.mysql.EventoDAOMySQL;
+
+/**
+ *
+ * @author rhau
+ */
+public class DBConfig {
+    
+    private final RegistroDAO registroDAO;
+           
+    public DBConfig() {
+        this.registroDAO = new EventoDAOMongo();
+        // this.registroDAO = new EventoDAOMySQL();
+    }
+
+    public RegistroDAO getRegistroDAO() {
+        return registroDAO;
+    }
+
+}
+    
+
